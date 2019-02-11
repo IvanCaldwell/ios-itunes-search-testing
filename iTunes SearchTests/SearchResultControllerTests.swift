c6f2b904735a834d8e4d4987fe6741695818a747
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-02-11 10:02:47 -0800

    Initial Commit

---
+++ iTunes SearchTests/SearchResultControllerTests.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultControllerTests.swift
+//  iTunes SearchTests
+//
+//  Created by Ivan Caldwell on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class SearchResultControllerTests: XCTestCase {
+
+    /**
+     What shall we test?
+     
+     - Does decoding work when given good data
+     - Does decoding fail when given bad data
+     - Does it build the correct URL?
+     - Does it build the correct URLRequest?
+     - Are results correctly saved into the property
+     - Is the completion handler called if the networking fails
+     - Is the completion handler called if the data is bad
+     - Is the completion handler called if the data is good
+     
+     **/
+    
+
+}
